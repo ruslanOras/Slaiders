fdfb2121e29b09612512c2c8de93c23bfe2e4a18
ruslanOras <78795288+ruslanOras@users.noreply.github.com>
2023-05-09 22:32:29 +0300

    Add files via upload

---
+++ 01-cards-sources/app.js
@@ -0,0 +1,21 @@
+function slidesPludin(activeSlide=0){
+        const slides = document.querySelectorAll('.slide')
+
+        slides[activeSlide].classList.add('active')
+
+    for (const slide of slides) {
+        slide.addEventListener('click', () => {
+            clearActiveClasses()
+
+            slide.classList.add('active')
+        })
+    }
+    function clearActiveClasses() {
+        slides.forEach( (slide) => {
+            slide.classList.remove('active')
+        })
+    }
+}
+slidesPludin()
+
+
